1f7064b15dbe724122e20aab07d766065a37e6b2
Nisargp17 <nisargsudhir6028@gmail.com>
2025-06-13 15:15:52 +0530

    Deploy commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import "./App.css";
 
 import NavBar from "./Components/NavBar";
 import Home from "./Components/Home";
-import Footer from "./Components/footer";
+import Footer from "./Components/Footer";
 import Menu from "./Components/Menu";
 import BookTable from "./Components/BookTable";
 import Contact from "./Components/Contact";
 import ReservationForm from "./Components/ReservationForm";
-import PaymentForm from "./Components/PaymentForm";
 
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
@@ -16,9 +15,24 @@ import { loadStripe } from "@stripe/stripe-js";
 const stripePromise = loadStripe(
   "pk_test_51RZS844CuzG9XgGrifLD1GYRqcB0jOh2EFXInNNTR9WIi6lSCSCJrLxnXZrD9AtmZNlwOPjz4iZw4WPLQabqM8zX00mGNirXNY"
 );
+
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const ScrollToTop = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return null;
+};
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
