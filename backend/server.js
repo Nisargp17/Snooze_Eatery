cb22a0a790d7c1a7c59dae424b8e3a048eb967d8
Nisargp17 <nisargsudhir6028@gmail.com>
2025-06-14 13:58:55 +0530

    Deploy commit

--- backend/server.js
+++ backend/server.js
@@ -36,14 +36,13 @@ app.post("/create-payment-intent", async (req, res) => {
 });
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("✅ Connected to local MongoDB");
-    app.listen(5000, () => {
-      console.log("🚀 Server running at http://localhost:5000");
+    console.log("✅ Connected to MongoDB Atlas");
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(
+        `🚀 Server running at http://localhost:${process.env.PORT || 5000}`
+      );
     });
   })
   .catch((err) => {
