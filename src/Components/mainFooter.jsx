2f06e10ec720291d044eebcd04fc476befd7de42
Nisargp17 <nisargsudhir6028@gmail.com>
2025-06-17 10:29:37 +0530

    Database commit

--- src/Components/mainFooter.jsx
+++ src/Components/mainFooter.jsx
@@ -1,4 +1,4 @@
-function Footer() {
+function mainFooter() {
   return (
     <footer className="bg-[#f8f8f8] text-gray-700 pt-10 pb-6 px-6 md:px-20">
       <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10">
@@ -89,4 +89,4 @@ function Footer() {
   );
 }
 
-export default Footer;
+export default mainFooter;
